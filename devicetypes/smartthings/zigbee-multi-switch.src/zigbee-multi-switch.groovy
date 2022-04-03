209165ff6f4ce3f6869f2a45545c861d71d1cf29
Muhammad Irfan koenadi <chiefasuka@gmail.com>
2022-04-03 12:48:10 +0800

    update

--- devicetypes/smartthings/zigbee-multi-switch.src/zigbee-multi-switch.groovy
+++ devicetypes/smartthings/zigbee-multi-switch.src/zigbee-multi-switch.groovy
@@ -278,49 +278,25 @@ private Boolean isOrvibo() {
 }
 
 private getChildCount() {
-	switch (device.getDataValue("model")) {
-		case "9f76c9f31b4c4a499e3aca0977ac4494":
-		case "HY0003":
-		case "HY0097":
-		case "HS2SW3L-EFR-3.0":
-		case "E220-KR3N0Z0-HA":
-		case "E220-KR3N0Z1-HA":
-		case "E220-KR3N0Z2-HA":
-		case "ZB-SW03":
-		case "JZ-ZB-003":
-		case "PM-S340-ZB":
-		case "PM-S340R-ZB":
-		case "PM-S350-ZB":
-		case "ST-S350-ZB":
-		case "SBM300Z3":
-		case "HS6SW3A-W-EF-3.0":
-			return 3
-		case "E220-KR4N0Z0-HA":
-		case "E220-KR4N0Z1-HA":
-		case "E220-KR4N0Z2-HA":
-		case "ZB-SW04":
-		case "JZ-ZB-004":
-		case "SBM300Z4":
-			return 4
-		case "E220-KR5N0Z0-HA":
-		case "E220-KR5N0Z1-HA":
-		case "E220-KR5N0Z2-HA":
-		case "ZB-SW05":
-		case "JZ-ZB-005":
-		case "SBM300Z5":
-			return 5
-		case "E220-KR6N0Z0-HA":
-		case "E220-KR6N0Z1-HA":
-		case "E220-KR6N0Z2-HA":
-		case "ZB-SW06":
-		case "JZ-ZB-006":
-		case "SBM300Z6":
-			return 6
-		case "E220-KR2N0Z0-HA":
-		case "E220-KR2N0Z1-HA":
-		case "E220-KR2N0Z2-HA":
-		case "SBM300Z2":
-		default:
-			return 2
+		if (device.getDataValue("model") == "9f76c9f31b4c4a499e3aca0977ac4494" || device.getDataValue("model") == "HY0003" || device.getDataValue("model") == "HY0097" || device.getDataValue("model") == "HS2SW3L-EFR-3.0" ) {
+		return 3
+	} else if (device.getDataValue("model") == "TS0601") {
+		return 2
+	} else if (device.getDataValue("model") == "E220-KR2N0Z0-HA") {
+		return 2
+	} else if (device.getDataValue("model") == "E220-KR3N0Z0-HA" || device.getDataValue("model") == "ZB-SW03") {
+		return 3
+	} else if (device.getDataValue("model") == "E220-KR4N0Z0-HA" || device.getDataValue("model") == "ZB-SW04") {
+		return 4
+	} else if (device.getDataValue("model") == "E220-KR5N0Z0-HA") {
+		return 5
+	} else if (device.getDataValue("model") == "E220-KR6N0Z0-HA") {
+		return 6 
+	} else if (device.getDataValue("model") == "PM-S340-ZB" || device.getDataValue("model") == "PM-S340R-ZB") {
+		return 3
+	} else if (device.getDataValue("model") == "" || device.getDataValue("model") == "PM-S340R-ZB") {
+		return 2
+	} else {
+		return 2
 	}
 }
